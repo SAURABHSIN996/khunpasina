bb9893d83b54ae68478cdd5b19f1f9cee034a0fc
Kvlkii <eximious99@gmail.com>
2021-12-07 22:55:00 +0530

    websiteipgymkvlkii

---
+++ ipgym-Kvlkii/src/Components/Featurebox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Featurebox(props) {
+    return (
+        <div className='a-box'>
+            <div class='a-b-img'>
+                <img src={props.image} aLt=''/>
+            </div>
+            <div className='a-b-text'>
+                <h2> {props.title} </h2>
+            </div>
+        </div>
+    )
+}
+
+export default Featurebox;
